556c5ca72c1dcb4318682be270338090ef76e8a9
vikar.aalam <vikar.aalam@M2B-L-4NTSYW2.dir.svc.accenture.com>
2021-09-21 09:48:19 +0530

    Change in Dao Class

--- AzureMaven2/src/main/java/com/lkm/dao/EmployeeDao.java
+++ AzureMaven2/src/main/java/com/lkm/dao/EmployeeDao.java
@@ -12,8 +12,8 @@ public class EmployeeDao {
 	public List<Employee> getAllEmployee(){
 		List<Employee> list= new ArrayList<Employee>();
 		list.add(new Employee(101, "Jhon", "Trainer", 20000.0));
-		list.add(new Employee(102, "Mark", "Developer", 25000.0));
-		list.add(new Employee(103, "Den", "Scrum", 3000.0));
+		list.add(new Employee(102, "Vakar", "Java_Developer", 25000.0));
+		list.add(new Employee(103, "Aalam", "Boot_Trainer", 3000.0));
 		return list;
 	}
 }
